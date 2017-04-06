ebd708a88eecba0f4dead17488c93f5262372a84
whhub <wanghui122004@me.com>
2017-04-06 15:55:16 +0800

    [PrismExplore][Version003][EventAggregationExplore]  SubjectViewModel & ObserverViewModel

---
+++ EventAggregationExplore/ViewModel/ObserverViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+
+namespace EventAggregationExplore.ViewModel
+{
+    public class ObserverViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<string> _events = new ObservableCollection<string>();
+        public ObservableCollection<string> Events {get {return _events;}} 
+    }
+}
---
+++ EventAggregationExplore/ViewModel/SubjectViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace EventAggregationExplore.ViewModel
+{
+    public class SubjectViewModel : ViewModelBase
+    {
+        #region [--LeftLabelCommand--]
+
+        private ICommand _leftLabelCommand;
+
+        public ICommand LeftLabelCommand
+        {
+            get { return _leftLabelCommand = _leftLabelCommand ?? new RelayCommand(OnLeftLabelPicked); }
+        }
+
+        private void OnLeftLabelPicked()
+        {
+            // TODO: Send out Event
+            MessageBox.Show("L");
+        }
+
+        #endregion [--LeftLabelCommand--]    
+
+
+        #region [--RightLabelCommand--]
+
+        private ICommand _rightLabelCommand;
+
+        public ICommand RightLabelCommand
+        {
+            get { return _rightLabelCommand = _rightLabelCommand ?? new RelayCommand(OnRightLabelPicked); }
+        }
+
+        private void OnRightLabelPicked()
+        {
+            // TODO: Send out Event
+            MessageBox.Show("R");           
+        }
+
+        #endregion [--RightLabelCommand--]    
+
+
+    }
+}
