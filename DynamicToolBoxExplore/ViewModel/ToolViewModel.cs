48524c862f3355abac900bdee2361be6a58302a2
whhub <wanghui122004@me.com>
2017-04-11 13:54:53 +0800

    [PrismExplore][Version021][DynamicToolBoxExplore] Tool.Command Binding to ToolViewModel.ToolCommand

--- DynamicToolBoxExplore/ViewModel/ToolViewModel.cs
+++ DynamicToolBoxExplore/ViewModel/ToolViewModel.cs
@@ -1,10 +1,28 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace DynamicToolBoxExplore.ViewModel
 {
     public abstract class ToolViewModel : ViewModelBase
     {
-         public abstract string Name { get; }
+        public abstract string Name { get; }
+
+        #region [--ToolCommand--]
+
+        private ICommand _toolCommand;
+
+        public ICommand ToolCommand
+        {
+            get { return _toolCommand = _toolCommand ?? new RelayCommand(OnClicked); }
+        }
+
+        protected abstract void OnClicked();
+
+        #endregion [--ToolCommand--]    
+
     }
 
     class RLabelViewModel : ToolViewModel
@@ -14,6 +32,11 @@ namespace DynamicToolBoxExplore.ViewModel
         public override string Name
         {
             get { return "R"; }
+        }
+
+        protected override void OnClicked()
+        {
+            MessageBox.Show("RLabel Picked");
         }
 
         #endregion
@@ -28,6 +51,11 @@ namespace DynamicToolBoxExplore.ViewModel
             get { return "M"; }
         }
 
+        protected override void OnClicked()
+        {
+            MessageBox.Show("MLabel Picked");
+        }
+
         #endregion
     }
 
@@ -38,6 +66,11 @@ namespace DynamicToolBoxExplore.ViewModel
         public override string Name
         {
             get { return "L"; }
+        }
+
+        protected override void OnClicked()
+        {
+            MessageBox.Show("LLabel Picked");
         }
 
         #endregion
