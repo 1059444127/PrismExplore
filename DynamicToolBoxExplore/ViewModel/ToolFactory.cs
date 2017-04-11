0ff62b8a2356fb9af05604b847b9c36e0ef2b843
whhub <wanghui122004@me.com>
2017-04-11 11:51:01 +0800

    [PrismExplore][Version020][DynamicToolBoxExplore] Create Tool  according to Config Attribute Available

--- DynamicToolBoxExplore/ViewModel/ToolFactory.cs
+++ DynamicToolBoxExplore/ViewModel/ToolFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace DynamicToolBoxExplore.ViewModel
@@ -12,10 +13,28 @@ namespace DynamicToolBoxExplore.ViewModel
             foreach (var e in xDoc.Descendants())
             {
                 //TODO-Later: ToolFactory.CreateToolsAccordingTo  Remove hard code.
+
+                // if e.Availabel == false , continue;
+                var availableAttribute = e.GetAttribute("Available");
+                bool available = true;
+                bool.TryParse(availableAttribute, out available);
+                if(!available) continue;
+
+                // Create ToolViewModel use reflection
                 var toolViewModelType = Type.GetType("DynamicToolBoxExplore.ViewModel." + e.Name + "ViewModel");
                 if(toolViewModelType == null) continue;
                 yield return (ToolViewModel)Activator.CreateInstance(toolViewModelType);
             }
+        }
+    }
+
+    public static class XElementExtension
+    {
+        public static string GetAttribute(this XElement element, string attributeName)
+        {
+            var attributes = element.Attributes();
+            var attribute = attributes.FirstOrDefault(a => a.Name.LocalName == attributeName);
+            return attribute == null ? string.Empty : attribute.Value;
         }
     }
 }
