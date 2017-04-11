0111020932d86e4d4e62afa3ee7585e8a3e5cf82
whhub <wanghui122004@me.com>
2017-04-11 11:25:11 +0800

    [PrismExplore][Version019][DynamicToolBoxExplore] Tool Binding to  ToolViewModel.Name

--- DynamicToolBoxExplore/ViewModel/ToolViewModel.cs
+++ DynamicToolBoxExplore/ViewModel/ToolViewModel.cs
@@ -2,20 +2,44 @@
 
 namespace DynamicToolBoxExplore.ViewModel
 {
-    public class ToolViewModel : ViewModelBase
+    public abstract class ToolViewModel : ViewModelBase
     {
-         
+         public abstract string Name { get; }
     }
 
     class RLabelViewModel : ToolViewModel
     {
+        #region Overrides of ToolViewModel
+
+        public override string Name
+        {
+            get { return "R"; }
+        }
+
+        #endregion
     }
 
     class MLabelViewModel : ToolViewModel
     {
+        #region Overrides of ToolViewModel
+
+        public override string Name
+        {
+            get { return "M"; }
+        }
+
+        #endregion
     }
 
     class LLabelViewModel : ToolViewModel
     {
+        #region Overrides of ToolViewModel
+
+        public override string Name
+        {
+            get { return "L"; }
+        }
+
+        #endregion
     }
 }
