ef70ac1a064360c2314bcab562bf515f7efd2fb1
whhub <wanghui122004@me.com>
2017-04-26 20:00:51 +0800

    [PrismExplore][Version034][MessengerExplore] MessengerViewer & ViewModel

---
+++ MessengerViewer/MessengerViewerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+
+namespace MessengerViewer
+{
+    public class MessengerViewerViewModel : ViewModelBase
+    {
+        #region [--AllowMessage--]
+
+        private bool _allowMessage = true;
+
+        public bool AllowMessage
+        {
+            get { return _allowMessage; }
+            set
+            {
+                if (_allowMessage == value) return;
+                _allowMessage = value;
+                RaisePropertyChanged(() => AllowMessage);
+            }
+        }
+
+        #endregion [--AllowMessage--]
+
+        private ObservableCollection<string> _messages = new ObservableCollection<string>();
+        public ObservableCollection<string> Messages {get { return _messages; }} 
+    }
+}
