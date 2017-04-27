f202c5c2f024e35c08108c1ff6ea8cd0cf760459
whhub <wanghui122004@me.com>
2017-04-27 15:26:11 +0800

    [PrismExplore][Version043][MultiInstanceImportExplore] Controller export as IController

--- MultiInstanceImportExplore/Controller.cs
+++ MultiInstanceImportExplore/Controller.cs
@@ -1,7 +1,20 @@
-﻿namespace MultiInstanceImportExplore
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+namespace MultiInstanceImportExplore
 {
-    public class Controller
+    [Export(typeof(IController))]
+    public class Controller : IController
     {
-         
+        private readonly IList<IView> _views = new List<IView>();
+
+        #region Implementation of IController
+
+        public void Register(IView view)
+        {
+            _views.Add(view);
+        }
+
+        #endregion
     }
 }
--- MultiInstanceImportExplore/IController.cs
+++ MultiInstanceImportExplore/IController.cs
@@ -2,5 +2,6 @@
 {
     interface IController
     {
+        void Register(IView view);
     }
 }
