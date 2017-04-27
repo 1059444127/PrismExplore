8b3c3524f5021260a67200addb96adef6b6cabc0
whhub <wanghui122004@me.com>
2017-04-27 11:20:13 +0800

    [PrismExplore][Version039][MessengerExplore] MessengerChannelHub as a singleton

---
+++ MessengerChannel/MessengerChannelHub.cs
@@ -0,0 +1,21 @@
+﻿namespace MessengerChannel
+{
+    public class MessengerChannelHub
+    {
+        #region [--Singleton--]
+
+        private static readonly MessengerChannelHub _instance = new MessengerChannelHub();
+
+        private MessengerChannelHub()
+        {
+            
+        }
+
+        public MessengerChannelHub Instance
+        {
+            get { return _instance; }
+        }
+
+        #endregion
+    }
+}
