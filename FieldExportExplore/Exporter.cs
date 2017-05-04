61b5116247c40c92be36ae5a228267f12bb90989
whhub <wanghui122004@me.com>
2017-05-04 17:16:00 +0800

    [PrismExplore][Version050][FieldExportExplore]

---
+++ FieldExportExplore/Exporter.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+
+namespace FieldExportExplore
+{
+    public class Exporter
+    {
+        [Export("SelfInitialized")] private string _s1 = "SelfInitialized";
+        [Export("ConstructorInitialized")] private string _s2;
+
+        public Exporter()
+        {
+            _s2 = "ConstructorInitialized";
+        }
+    }
+}
---
+++ FieldExportExplore/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+
+namespace FieldExportExplore
+{
+    class Program
+    {
+        [Import("SelfInitialized")] string S1;
+        [Import("ConstructorInitialized")] string S2;
+
+        public Program()
+        {
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            var container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            Console.WriteLine("SelfInitialized={0}, ConstructorInitialized={1}", p.S1, p.S2);
+        }
+    }
+}
